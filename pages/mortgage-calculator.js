4a68eb3cdf4990120ec862eee8a3d9e20e7ecd90
usmanmauser <usmanibrahimmauser@gmail.com>
2025-04-11 06:09:51 +0530

    Update mortgage-calculator.js

--- pages/mortgage-calculator.js
+++ pages/mortgage-calculator.js
@@ -2,8 +2,8 @@
 
 import { useState } from 'react'
 import Head from 'next/head'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
 
 export default function MortgageCalculator() {
