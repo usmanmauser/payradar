3095217ce538ead9141b8e13287cdff1f516f918
usmanmauser <usmanibrahimmauser@gmail.com>
2025-04-11 06:06:46 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import "@/styles/globals.css";
-import Head from "next/head";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import '../styles/globals.css';
+import Head from 'next/head';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }) {
   return (
