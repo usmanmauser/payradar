4fca83decaf7ecf2a3ba7c72f5587ed4369ec1d9
usmanmauser <usmanibrahimmauser@gmail.com>
2025-04-11 06:09:37 +0530

    Update loan-repayment.js

--- pages/loan-repayment.js
+++ pages/loan-repayment.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Head from 'next/head'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import {
   ResponsiveContainer,
   LineChart,
