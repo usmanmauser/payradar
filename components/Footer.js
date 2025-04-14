09879bf46734f314cbcdc6bd9a701fd5cb39dc49
usmanmauser <usmanibrahimmauser@gmail.com>
2025-04-14 05:35:10 +0530

    Delete components directory

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 mt-10 border-t border-gray-300">
-      <div className="max-w-6xl mx-auto px-4 py-6 text-center text-sm text-gray-600">
-        © {new Date().getFullYear()} PayRadar. All rights reserved.
-      </div>
-    </footer>
-  );
-}
